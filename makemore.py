ccb22952a8bdb1127e866dac7b60b4c0868a75ec
berilav <vlad.berila@autodesk.com>
2024-04-26 12:34:31 +0300

    add model

--- makemore.py
+++ makemore.py
@@ -18,7 +18,7 @@ itos = {i:s for s,i in stoi.items()}
 
 block_size = 3 # context length: how many characters do we take to predict the next one?
 X, Y = [], []
-for w in words[:5]:
+for w in words:
     context = [0] * block_size
     for ch in w + '.':
         ix = stoi[ch]
@@ -29,4 +29,44 @@ for w in words[:5]:
 X = torch.tensor(X)
 Y = torch.tensor(Y)
 
-C = torch.randn((27,2)) # initialize the character embeddings randomly
+g = torch.Generator().manual_seed(2147483647) # seed the random number generator
+C = torch.randn((27,2), generator= g) # initialize the character embeddings randomly
+W1 = torch.randn((6,100), generator= g) # initialize the weights randomly
+b1 = torch.randn(100, generator= g) # initialize the bias randomly
+W2 = torch.randn((100,27), generator= g) # initialize the weights randomly
+b2 = torch.randn(27, generator= g) # initialize the bias randomly
+parameters = [C, W1, b1, W2, b2] # collect all parameters
+
+for p in parameters:
+    p.requires_grad = True # setting requires_grad=True
+
+learningStepExponent = torch.linspace(-3, 0, 1000)
+learningStep = 10**learningStepExponent
+lri = []
+lossi = []
+
+for i in range(1000): # train for 1000 iterations
+    # minibatch construction
+    ix = torch.randint(0, X.shape[0], (32,)) # random indices
+
+    # forward pass
+    emb = C[X[ix]]
+    h = torch.tanh(emb.view(-1,6) @ W1 + b1) # compute hidden states
+    logits = h @ W2 + b2 # compute logits
+    #counts = logits.exp() # compute the softmax
+    #prob = counts / counts.sum(-1, keepdims=True) # normalize to get a probability
+    #loss = -prob[torch.arange(32), Y].log().mean() # cross-entropy loss
+    loss = F.cross_entropy(logits, Y[ix])
+    print(loss.item())
+
+    # backward pass
+    for p in parameters:
+        p.grad = None
+    loss.backward() # autograd computes all the gradients
+
+    # update
+    lr = learningStep[i]
+    for p in parameters:
+        p.data +=(-lr * p.grad) # perform a GD step
+
+# print(loss.item())
