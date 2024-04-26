0a4d83de00e5e1e56fc3f326b865a89ed7e06be6
berilav <vlad.berila@autodesk.com>
2024-04-26 10:52:01 +0300

    fix paths

--- makemore.py
+++ makemore.py
@@ -1,9 +1,12 @@
 import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt # for making figures
+import pathlib # for dealing with file paths
+
+path = pathlib.Path().resolve() # get the path to the current directory
 
 #read in all the words
-words = open('/Users/vladberila/Desktop/makemore MLP/names.txt', 'r').read().splitlines()
+words = open(path.joinpath('names.txt'), 'r').read().splitlines()
 
 # build the vocabulary of characters and mappings to/from integers
 chars = sorted(list(set(''.join(words))))
@@ -16,15 +19,14 @@ itos = {i:s for s,i in stoi.items()}
 block_size = 3 # context length: how many characters do we take to predict the next one?
 X, Y = [], []
 for w in words[:5]:
-    print(w)
     context = [0] * block_size
     for ch in w + '.':
         ix = stoi[ch]
         X.append(context)
         Y.append(ix)
-        print(''.join(itos[i] for i in context), '---->', itos[ix])
         context = context[1:] + [ix] # crop and append
 
 X = torch.tensor(X)
 Y = torch.tensor(Y)
 
+C = torch.randn((27,2)) # initialize the character embeddings randomly
