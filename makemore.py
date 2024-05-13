de4e01a084d62d4ef1459fa7e0ab6e6cc8dd3b94
berilav <vlad.berila@autodesk.com>
2024-05-13 18:00:36 +0300

    pui it in classes

--- makemore.py
+++ makemore.py
@@ -5,13 +5,6 @@ import pathlib # for dealing with file paths
 import random # for shuffling the data
 
 path = pathlib.Path().resolve() # get the path to the current directory
-
-block_size = 3 # context length: how many characters do we take to predict the next one?
-batch_size = 32 
-vocab_size = 27 # 26 letters + 1 period
-n_embed = 10 # embedding size
-n_hidden = 200 # hidden size
-
 #read in all the words
 words = open(path.joinpath('names.txt'), 'r').read().splitlines()
 
@@ -20,50 +13,157 @@ chars = sorted(list(set(''.join(words))))
 stoi = {s:i+1 for i,s in enumerate(chars)}
 stoi['.'] = 0
 itos = {i:s for s,i in stoi.items()}
+vocab_size = len(itos)
 
 # build the dataset
-def build_dataset(words):
-    X, Y = [], []
-    for w in words:
-        context = [0] * block_size
-        for ch in w + '.':
-            ix = stoi[ch]
-            X.append(context)
-            Y.append(ix)
-            context = context[1:] + [ix] # crop and append
+block_size = 8 # context length: how many characters do we take to predict the next one?
 
-    X = torch.tensor(X)
-    Y = torch.tensor(Y)
-    return X, Y
+def build_dataset(words):  
+  X, Y = [], []
+  
+  for w in words:
+    context = [0] * block_size
+    for ch in w + '.':
+      ix = stoi[ch]
+      X.append(context)
+      Y.append(ix)
+      context = context[1:] + [ix] # crop and append
 
-# 80% training set, 10% dev/validation set, 10% test set
-n1 = int(0.8 * len(words))
-n2 = int(0.9 * len(words))
-random.seed(42) # seed the random number generator
-random.shuffle(words)
+  X = torch.tensor(X)
+  Y = torch.tensor(Y)
+  print(X.shape, Y.shape)
+  return X, Y
 
-Xtr, Ytr = build_dataset(words[:n1])
-Xdev, Ydev = build_dataset(words[n1:n2])
-Xte, Yte = build_dataset(words[n2:])
+n1 = int(0.8*len(words))
+n2 = int(0.9*len(words))
+Xtr,  Ytr  = build_dataset(words[:n1])     # 80%
+Xdev, Ydev = build_dataset(words[n1:n2])   # 10%
+Xte,  Yte  = build_dataset(words[n2:])     # 10%
 
-# MLP revisited
-n_embd = 10 # the dimensionality of the character embedding vectors
-n_hidden = 200 # the number of neurons in the hidden layer of the MLP
+# -----------------------------------------------------------------------------------------------
+class Linear:
+  
+  def __init__(self, fan_in, fan_out, bias=True):
+    self.weight = torch.randn((fan_in, fan_out)) / fan_in**0.5 # note: kaiming init
+    self.bias = torch.zeros(fan_out) if bias else None
+  
+  def __call__(self, x):
+    self.out = x @ self.weight
+    if self.bias is not None:
+      self.out += self.bias
+    return self.out
+  
+  def parameters(self):
+    return [self.weight] + ([] if self.bias is None else [self.bias])
 
-g = torch.Generator().manual_seed(2147483647) # for reproducibility
-C  = torch.randn((vocab_size, n_embd),            generator=g)
-W1 = torch.randn((n_embd * block_size, n_hidden), generator=g) * (5/3)/((n_embd * block_size)**0.5) #* 0.2
-#b1 = torch.randn(n_hidden,                        generator=g) * 0.01
-W2 = torch.randn((n_hidden, vocab_size),          generator=g) * 0.01
-b2 = torch.randn(vocab_size,                      generator=g) * 0
+# -----------------------------------------------------------------------------------------------
+class BatchNorm1d:
+  
+  def __init__(self, dim, eps=1e-5, momentum=0.1):
+    self.eps = eps
+    self.momentum = momentum
+    self.training = True
+    # parameters (trained with backprop)
+    self.gamma = torch.ones(dim)
+    self.beta = torch.zeros(dim)
+    # buffers (trained with a running 'momentum update')
+    self.running_mean = torch.zeros(dim)
+    self.running_var = torch.ones(dim)
+  
+  def __call__(self, x):
+    # calculate the forward pass
+    if self.training:
+      if x.ndim == 2:
+        dim = 0
+      elif x.ndim == 3:
+        dim = (0,1)
+      xmean = x.mean(dim, keepdim=True) # batch mean
+      xvar = x.var(dim, keepdim=True) # batch variance
+    else:
+      xmean = self.running_mean
+      xvar = self.running_var
+    xhat = (x - xmean) / torch.sqrt(xvar + self.eps) # normalize to unit variance
+    self.out = self.gamma * xhat + self.beta
+    # update the buffers
+    if self.training:
+      with torch.no_grad():
+        self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * xmean
+        self.running_var = (1 - self.momentum) * self.running_var + self.momentum * xvar
+    return self.out
+  
+  def parameters(self):
+    return [self.gamma, self.beta]
 
-# BatchNorm parameters
-bngain = torch.ones((1, n_hidden))
-bnbias = torch.zeros((1, n_hidden))
-bnmean_running = torch.zeros((1, n_hidden))
-bnstd_running = torch.ones((1, n_hidden))
+# -----------------------------------------------------------------------------------------------
+class Tanh:
+  def __call__(self, x):
+    self.out = torch.tanh(x)
+    return self.out
+  def parameters(self):
+    return []
 
-parameters = [C, W1, W2, b2, bngain, bnbias]
+# -----------------------------------------------------------------------------------------------
+class Embedding:
+  
+  def __init__(self, num_embeddings, embedding_dim):
+    self.weight = torch.randn((num_embeddings, embedding_dim))
+    
+  def __call__(self, IX):
+    self.out = self.weight[IX]
+    return self.out
+  
+  def parameters(self):
+    return [self.weight]
+
+# -----------------------------------------------------------------------------------------------
+class FlattenConsecutive:
+  
+  def __init__(self, n):
+    self.n = n
+    
+  def __call__(self, x):
+    B, T, C = x.shape
+    x = x.view(B, T//self.n, C*self.n)
+    if x.shape[1] == 1:
+      x = x.squeeze(1)
+    self.out = x
+    return self.out
+  
+  def parameters(self):
+    return []
+
+# -----------------------------------------------------------------------------------------------
+class Sequential:
+  
+  def __init__(self, layers):
+    self.layers = layers
+  
+  def __call__(self, x):
+    for layer in self.layers:
+      x = layer(x)
+    self.out = x
+    return self.out
+  
+  def parameters(self):
+    # get parameters of all layers and stretch them out into one list
+    return [p for layer in self.layers for p in layer.parameters()]
+  
+
+n_embd = 24 # the dimensionality of the character embedding vectors
+n_hidden = 128 # the number of neurons in the hidden layer of the MLP
+model = Sequential([
+  Embedding(vocab_size, n_embd),
+  FlattenConsecutive(2), Linear(n_embd * 2, n_hidden, bias=False), BatchNorm1d(n_hidden), Tanh(),
+  FlattenConsecutive(2), Linear(n_hidden*2, n_hidden, bias=False), BatchNorm1d(n_hidden), Tanh(),
+  FlattenConsecutive(2), Linear(n_hidden*2, n_hidden, bias=False), BatchNorm1d(n_hidden), Tanh(),
+  Linear(n_hidden, vocab_size),
+])
+
+# parameter init
+with torch.no_grad():
+  model.layers[-1].weight *= 0.1 # last layer make less confident
+
+parameters = model.parameters()
 print(sum(p.nelement() for p in parameters)) # number of parameters in total
 for p in parameters:
   p.requires_grad = True
@@ -75,26 +175,11 @@ lossi = []
 for i in range(max_steps):
   
   # minibatch construct
-  ix = torch.randint(0, Xtr.shape[0], (batch_size,), generator=g)
+  ix = torch.randint(0, Xtr.shape[0], (batch_size,))
   Xb, Yb = Xtr[ix], Ytr[ix] # batch X,Y
   
   # forward pass
-  emb = C[Xb] # embed the characters into vectors
-  embcat = emb.view(emb.shape[0], -1) # concatenate the vectors
-  # Linear layer
-  hpreact = embcat @ W1 #+ b1 # hidden layer pre-activation
-  # BatchNorm layer
-  # -------------------------------------------------------------
-  bnmeani = hpreact.mean(0, keepdim=True)
-  bnstdi = hpreact.std(0, keepdim=True)
-  hpreact = bngain * (hpreact - bnmeani) / bnstdi + bnbias
-  with torch.no_grad():
-    bnmean_running = 0.999 * bnmean_running + 0.001 * bnmeani
-    bnstd_running = 0.999 * bnstd_running + 0.001 * bnstdi
-  # -------------------------------------------------------------
-  # Non-linearity
-  h = torch.tanh(hpreact) # hidden layer
-  logits = h @ W2 + b2 # output layer
+  logits = model(Xb)
   loss = F.cross_entropy(logits, Yb) # loss function
   
   # backward pass
@@ -102,8 +187,8 @@ for i in range(max_steps):
     p.grad = None
   loss.backward()
   
-  # update
-  lr = 0.1 if i < 100000 else 0.01 # step learning rate decay
+  # update: simple SGD
+  lr = 0.1 if i < 150000 else 0.01 # step learning rate decay
   for p in parameters:
     p.data += -lr * p.grad
 
@@ -112,22 +197,41 @@ for i in range(max_steps):
     # print(f'{i:7d}/{max_steps:7d}: {loss.item():.4f}')
   lossi.append(loss.log10().item())
 
-@torch.no_grad() # this decorator disables gradient tracking
+# put layers into eval mode (needed for batchnorm especially)
+for layer in model.layers:
+  layer.training = False
+
+# evaluate the loss
+@torch.no_grad() # this decorator disables gradient tracking inside pytorch
 def split_loss(split):
   x,y = {
     'train': (Xtr, Ytr),
     'val': (Xdev, Ydev),
     'test': (Xte, Yte),
   }[split]
-  emb = C[x] # (N, block_size, n_embd)
-  embcat = emb.view(emb.shape[0], -1) # concat into (N, block_size * n_embd)
-  hpreact = embcat @ W1 # + b1
-  #hpreact = bngain * (hpreact - hpreact.mean(0, keepdim=True)) / hpreact.std(0, keepdim=True) + bnbias
-  hpreact = bngain * (hpreact - bnmean_running) / bnstd_running + bnbias
-  h = torch.tanh(hpreact) # (N, n_hidden)
-  logits = h @ W2 + b2 # (N, vocab_size)
+  logits = model(x)
   loss = F.cross_entropy(logits, y)
   print(split, loss.item())
 
 split_loss('train')
 split_loss('val')
+
+# sample from the model
+for _ in range(20):
+    
+    out = []
+    context = [0] * block_size # initialize with all ...
+    while True:
+      # forward pass the neural net
+      logits = model(torch.tensor([context]))
+      probs = F.softmax(logits, dim=1)
+      # sample from the distribution
+      ix = torch.multinomial(probs, num_samples=1).item()
+      # shift the context window and track the samples
+      context = context[1:] + [ix]
+      out.append(ix)
+      # if we sample the special '.' token, break
+      if ix == 0:
+        break
+    
+    print(''.join(itos[i] for i in out)) # decode and print the generated word
